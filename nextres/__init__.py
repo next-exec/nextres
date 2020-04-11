8bb655900569254ad7cb6d52537a923ebc33d98e
Michael William Le Nguyen <michael@mail.ttp.codes>
2020-04-11 01:18:34 -0700

    Index page

--- nextres/__init__.py
+++ nextres/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_login import login_required
 
 from nextres.controllers import *
 from nextres.database import Database
@@ -17,3 +18,10 @@ database = Database(app)
 
 # Initialize controllers.
 auth = AuthController(app)
+
+
+@app.route('/')
+@login_required
+@auth.authorize.in_group('residents')
+def index():
+    return render_template('index.html')
