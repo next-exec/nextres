7b09572b4443906ba778390510d6e576f69d324d
Michael William Le Nguyen <michael@mail.ttp.codes>
2020-04-11 00:09:12 -0700

    Set up SQLAlchemy

--- nextres/__init__.py
+++ nextres/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
+from nextres.database import Database
+
 from json import load
 
 app = Flask(__name__)
 
 with open('config.json') as fp:
     config = load(fp)
+app.config.update(SQLALCHEMY_DATABASE_URI=config['database'], SQLALCHEMY_TRACK_MODIFICATIONS=True)
+
+# Initialize database.
+database = Database(app)
---
+++ nextres/database/__init__.py
@@ -0,0 +1,8 @@
+from nextres.database.util import db
+
+
+class Database:
+    def __init__(self, app):
+        db.init_app(app)
+        with app.app_context():
+            db.create_all()
