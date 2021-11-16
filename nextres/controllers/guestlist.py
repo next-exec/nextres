3fcba2c8cfd05192b36422024de24f5401a310e2
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-11-16 02:52:04 -0500

    guestlist: validate kerberoi

--- nextres/controllers/guestlist.py
+++ nextres/controllers/guestlist.py
@@ -1,13 +1,21 @@
 from flask import flash, render_template, request
 from flask_login import current_user, login_required
+from requests import get
 
 from nextres.constants import FLASH_ERROR, FLASH_SUCCESS
 from nextres.controllers.auth import AuthController
 from nextres.database import db
 from nextres.database.models.guest import Guest, GuestListType
 
+from re import fullmatch
+
 class GuestListController:
     def __init__(self, app):
+        authorization = {
+            'client_id': app.config['PEOPLE_API_CLIENT_ID'],
+            'client_secret': app.config['PEOPLE_API_CLIENT_SECRET']
+        }
+
         @app.route('/guestlist', methods=['GET', 'POST'])
         @login_required
         @AuthController.instance.authorize.in_group('residents')
@@ -44,6 +52,13 @@ class GuestListController:
                             continue
                         if not fullmatch('[a-z0-9]*', kerberos):
                             entry[2] = 'kerberos must only contain lowercase letters and numbers'
+                            continue
+                        # f-strings don't appear until python 3.6. scripts is stuck on python 3.3.
+                        r = get('https://mit-people-v3.cloudhub.io/people/v3/people/{}'.format(kerberos), headers=authorization)
+                        data = r.json()
+                        affiliation = data['item']['affiliations'][0]
+                        if r.status_code != 200 or affiliation['type'] != 'student' or affiliation['classYear'] == 'G':
+                            entry[2] = 'kerberos must belong to a current mit undergrad'
                             continue
                         guests.append(Guest(kerberos=kerberos, name=name, list_type=GuestListType.Desk))
                     if any(map(lambda entry: entry[2], entries)):
