7390463ca142f946ca583739489299b762534caf
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-11-16 03:40:02 -0500

    license: add agpl-3.0-only license header

--- nextres/controllers/guestlist.py
+++ nextres/controllers/guestlist.py
@@ -1,3 +1,19 @@
+# spdx-license-identifier: agpl-3.0-only
+#
+# Copyright (C) 2021 Next Exec
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>
+
 from flask import flash, render_template, request
 from flask_login import current_user, login_required
 from requests import get
