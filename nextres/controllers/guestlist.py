3d68bec92a94cdc6953266dbbaa7174b6f16de79
jennifer19-meet <sunflowermaster234@yahoo.com>
2022-02-21 14:54:45 -0500

    minor bug fix

--- nextres/controllers/guestlist.py
+++ nextres/controllers/guestlist.py
@@ -150,7 +150,7 @@ class GuestListController:
                         entry[2] = 'kerberos must belong to a current student'
                         continue
                     guests.append(Guest(kerberos=kerberos, name=name, phone=phone, list_type=GuestListType.Desk))
-                if any(map(lambda entry: entry[3], entries)) or any(map(lambda express_entry: express_entry[2], express_entries)):
+                if any(map(lambda entry: entry[3], entries)) or any(map(lambda express_entry: express_entry[3], express_entries)):
                     flash('An invalid guestlist was received. See below.', FLASH_ERROR)
                     return ctx.return_response()
                 current_user.guests = guests + express_guests
