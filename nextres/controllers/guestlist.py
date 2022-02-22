9cfe2be1761c217532f90f6a984d0991a04156cf
jennifer19-meet <sunflowermaster234@yahoo.com>
2022-02-21 20:10:24 -0500

    revert to original working version

--- nextres/controllers/guestlist.py
+++ nextres/controllers/guestlist.py
@@ -53,8 +53,7 @@ class GuestListController:
                   return render_template('guestlists/edit.html',
                                    existing=None,
                                    desk=current_user.guests.filter_by(list_type=GuestListType.Desk).all(),
-                                   express=current_user.guests.filter_by(list_type=GuestListType.Express).all(),
-                                   editable = db.session.query(Settings).filter(express_guest_editable= True).first())
+                                   express=current_user.guests.filter_by(list_type=GuestListType.Express).all())
             else:
                 ctx = ResponseContext('guestlists/edit.html', {
                     'existing': None,
